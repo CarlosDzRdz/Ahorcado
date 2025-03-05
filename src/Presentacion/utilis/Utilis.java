42d4a8d80b179ce255a5e81bddf984f8465c48f5
CarlosDzRdz <cadr2627@gmail.com>
2025-03-05 12:50:23 -0600

    Se agrega el proyecto de ahorcado

---
+++ src/Presentacion/utilis/Utilis.java
@@ -0,0 +1,10 @@
+package Presentacion.utilis;
+
+public class Utilis {
+
+    public static void limpiarPantalla() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+}
