8a444df9adf2a539af80937a9e0688979c64700d
CarlosDzRdz <cadr2627@gmail.com>
2025-03-16 20:56:44 -0600

    Ahorcado final 2.0

--- src/Presentacion/Obeserver/Tablero.java
+++ src/Presentacion/Obeserver/Tablero.java
@@ -14,41 +14,8 @@ public class Tablero {
     private static final String[] PALABRAS = {"Mexico", "Java", "Docker", "GitHub", "SpringBoot", "BaseDatos", "Servidor", "Internet", "Framework", "Seguridad"};
     private int oportunidades;
     private StringBuilder letrasIngresadas;
-    private String head1 = "", body1 = " ", lefta1 = " ", righta1 = " ", leftf1 = " ", rightf1 = " ";
     private String head, body, lefta, righta, leftf, rightf;
     String [] tablero;
-
-    public String[] getTablero() {
-        return tablero;
-    }
-
-    public void setTablero(String[] tablero) {
-        this.tablero = tablero;
-    }
-
-    public int getOportunidades() {
-        return oportunidades;
-    }
-
-    public void setOportunidades(int oportunidades) {
-        this.oportunidades = oportunidades;
-    }
-
-    public StringBuilder getLetrasIngresadas() {
-        return letrasIngresadas;
-    }
-
-    public void setLetrasIngresadas(StringBuilder letrasIngresadas) {
-        this.letrasIngresadas = letrasIngresadas;
-    }
-
-    public String getPalabraSecreta() {
-        return palabraSecreta;
-    }
-
-    public void setPalabraSecreta(String palabraSecreta) {
-        this.palabraSecreta = palabraSecreta;
-    }
 
     public Tablero() {
         this.observadores = new ArrayList<>();
@@ -57,11 +24,10 @@ public class Tablero {
         this.palabraSecreta = PALABRAS[random.nextInt(PALABRAS.length)];
         this.espacios = new StringBuilder("_".repeat(palabraSecreta.length()));
         this.oportunidades = 0;
-        this.letrasIngresadas = new StringBuilder(); // Inicializamos como StringBuilder
-        this.head = "O"; this.body = "|"; this.lefta = "\\"; this.righta = "/"; this.leftf = "\\"; this.rightf = "/";
+        this.letrasIngresadas = new StringBuilder();
+        this.head = " "; this.body = " "; this.lefta = " "; this.righta = " "; this.leftf = " "; this.rightf = " ";
         this.tablero = new String[] {head, body, lefta, righta, leftf, rightf};
     }
-
 
     public void attach(Observer observer) {
         observadores.add(observer);
@@ -74,23 +40,21 @@ public class Tablero {
     }
 
     public void dibujarMonito() {
-
-        // Actualiza el estado acumulado en función de las oportunidades
         switch (oportunidades) {
-            case 1 -> head1 = tablero[0];
-            case 2 -> body1 = tablero[1];
-            case 3 -> lefta1 = tablero[2];
-            case 4 -> righta1 = tablero[3];
-            case 5 -> leftf1 = tablero[4];
-            case 6 -> rightf1 = tablero[5];
+            case 1 -> tablero[0] = "O"; // Cabeza
+            case 2 -> tablero[1] = "|"; // Cuerpo
+            case 3 -> tablero[2] = "/"; // Brazo izquierdo (de frente)
+            case 4 -> tablero[3] = "\\"; // Brazo derecho (de frente)
+            case 5 -> tablero[4] = "/"; // Pierna izquierda (de frente)
+            case 6 -> tablero[5] = "\\"; // Pierna derecha (de frente)
         }
 
-        // Imprime el estado acumulado del monito
+        // Imprime el estado actual del monito
         System.out.println("      ~");
         System.out.println("      |");
-        System.out.println("      " + head1);
-        System.out.println("     " + righta1 + body1 + lefta1);
-        System.out.println("     " + rightf1 + " " + leftf1);
+        System.out.println("      " + tablero[0]);
+        System.out.println("     " + tablero[2] + tablero[1] + tablero[3]);
+        System.out.println("     " + tablero[4] + " " + tablero[5]);
 
         // Muestra los espacios de la palabra
         for (int i = 0; i < espacios.length(); i++) {
@@ -135,6 +99,38 @@ public class Tablero {
         } else if (intentosRestantes == 0) {
             System.out.println("\n¡Perdiste! La palabra era: " + palabraSecreta);
         }
+    }
+
+    public String[] getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(String[] tablero) {
+        this.tablero = tablero;
+    }
+
+    public int getOportunidades() {
+        return oportunidades;
+    }
+
+    public void setOportunidades(int oportunidades) {
+        this.oportunidades = oportunidades;
+    }
+
+    public StringBuilder getLetrasIngresadas() {
+        return letrasIngresadas;
+    }
+
+    public void setLetrasIngresadas(StringBuilder letrasIngresadas) {
+        this.letrasIngresadas = letrasIngresadas;
+    }
+
+    public String getPalabraSecreta() {
+        return palabraSecreta;
+    }
+
+    public void setPalabraSecreta(String palabraSecreta) {
+        this.palabraSecreta = palabraSecreta;
     }
 
 }
